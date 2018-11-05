a32a1bb1d3632197dca33208879567083c489653
Joshua Dean <15590534@students.lincoln.ac.uk>
2018-11-05 15:24:41 +0000

    Used volley for a simple request

--- app/src/main/java/com/example/joshuadean/movieapp/searchIntent.java
+++ app/src/main/java/com/example/joshuadean/movieapp/searchIntent.java
@@ -5,6 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class searchIntent extends AppCompatActivity {
     //make variable for input
     private TextInputLayout textInputMovie;
@@ -20,9 +27,29 @@ public class searchIntent extends AppCompatActivity {
         String movieRawInput = textInputMovie.getEditText().getText().toString();
         //convert it to movie api standard
         String movieInput = movieRawInput.replace(" ", "+");
+        //Convert the movie input into a URL that works with the movie api
         String URL = "http://www.omdbapi.com/?t=" + movieInput + "&apikey=567b015";
 
+        //Make a volley request queue
+        RequestQueue queue = Volley.newRequestQueue(this);
 
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        String responsed =  response.substring(0,5000);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
 
 
     }
