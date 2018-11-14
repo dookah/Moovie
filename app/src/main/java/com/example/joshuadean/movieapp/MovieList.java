840f42a517638678a6fd2e3f84d595712a4278b5
Joshua Dean <15590534@students.lincoln.ac.uk>
2018-11-14 20:30:46 +0000

    Handle onclick of listview in movie list

--- app/src/main/java/com/example/joshuadean/movieapp/MovieList.java
+++ app/src/main/java/com/example/joshuadean/movieapp/MovieList.java
@@ -2,8 +2,12 @@ package com.example.joshuadean.movieapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.reactiveandroid.query.Select;
 
@@ -20,6 +24,7 @@ public class MovieList extends AppCompatActivity {
         ListView lv = (ListView) findViewById(R.id.lv);
 
         List<movieDatabase> movies = Select.from(movieDatabase.class).fetch();
+
         List<String> movieTitles = new ArrayList<String>();
 
         for(int i = 0; i < movies.size(); i++){
@@ -33,5 +38,17 @@ public class MovieList extends AppCompatActivity {
 
         lv.setAdapter(arrayAdapter);
 
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //get the text of the clicked element
+                String item = ((TextView)view).getText().toString();
+                //TODO On click open a page where it displays info about the movie and we can delete it from the database!
+
+
+                Toast.makeText(getBaseContext(), item, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
