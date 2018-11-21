8e5056b818c703aba5b405c0257cfc0fc93b07a5
Joshua Dean <15590534@students.lincoln.ac.uk>
2018-11-21 16:42:34 +0000

    Added swipe menu to the watch list

--- app/src/main/java/com/example/joshuadean/movieapp/watchActivity.java
+++ app/src/main/java/com/example/joshuadean/movieapp/watchActivity.java
@@ -1,6 +1,9 @@
 package com.example.joshuadean.movieapp;
 
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +11,12 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.reactiveandroid.query.Select;
 
 import java.util.ArrayList;
@@ -21,11 +29,11 @@ public class watchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch);
 
-        ListView lv = (ListView) findViewById(R.id.lv);
+        SwipeMenuListView lv = (SwipeMenuListView)findViewById(R.id.lv);
 
         List<watchDatabase> movies = Select.from(watchDatabase.class).fetch();
 
-        List<String> movieTitles = new ArrayList<String>();
+        final List<String> movieTitles = new ArrayList<String>();
 
         for(int i = 0; i < movies.size(); i++){
             movieTitles.add(movies.get(i).getMovie().getMovieTitle());
@@ -37,17 +45,62 @@ public class watchActivity extends AppCompatActivity {
                 movieTitles);
 
         lv.setAdapter(arrayAdapter);
+        //Make instance of swipe menu library
+        //Boilerplate code from SwipeMenuListView Library Documentation - https://github.com/baoyongzhang/SwipeMenuListView
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+        //Create swipe menu options
+            @Override
+            public void create(SwipeMenu menu) {
+                // create "open" item
+                SwipeMenuItem openItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                openItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9,
+                        0xCE)));
+                // set item width
+                openItem.setWidth(250);
+                // set item title
+                openItem.setTitle("Seen");
+                // set item title fontsize
+                openItem.setTitleSize(18);
+                // set item title font color
+                openItem.setTitleColor(Color.WHITE);
+                // add to menu
+                menu.addMenuItem(openItem);
+            }
+        };
 
+        // set creator
+        lv.setMenuCreator(creator);
+
+        //handle the clicks
+        //Boilerplate code from SwipeMenuListView Library Documentation - https://github.com/baoyongzhang/SwipeMenuListView
+        lv.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                //get the clicked title
+                String item = movieTitles.get(position);
+                //When you click the first button
+                switch (index) {
+                    case 0:
+                        //TODO Add the movie to the seen database
+                        //TODO Remove movie from to watch database
+                        //TODO Return home
+                        break;
+                }
+                return false;
+            }
+        });
+
+        //called when you click on one of the list view items
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                //get the text of the clicked element
-                String item = ((TextView)view).getText().toString();
-                //TODO On click open a page where it displays info about the movie and we can delete it from the database!
+                //get the clicked title by getting the position in the array
+                String item = movieTitles.get(position);
+                Toast.makeText(watchActivity.this, item, Toast.LENGTH_SHORT).show();
                 //new intent for movie info
                 Intent intent = new Intent(view.getContext(), movieInfo.class);
-
                 //Create a new bundle to hold the movie information
                 Bundle movieInfo = new Bundle();
                 //put the clicked title in the bundle
@@ -59,8 +112,9 @@ public class watchActivity extends AppCompatActivity {
                 startActivity(intent);
                 //Go back to the home page if the movie info intent finishes
                 finish();
-
             }
         });
+
+
     }
 }
